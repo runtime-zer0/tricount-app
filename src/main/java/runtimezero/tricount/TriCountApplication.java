3c83230d58a90868e38e46efc74caa9a2b6707f4
runtime-zer0 <bigstring.zero@gmail.com>
2024-05-09 15:54:00 +0900

    init: 프로젝트 초기 설정

---
+++ src/main/java/runtimezero/tricount/TriCountApplication.java
@@ -0,0 +1,13 @@
+package runtimezero.tricount;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TriCountApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TriCountApplication.class, args);
+    }
+
+}
